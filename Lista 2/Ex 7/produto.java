10318cd035c20ef6eb9e58769f36581ccfdb5a73
brunodecnop <78354706+brunodecnop@users.noreply.github.com>
2021-06-28 18:31:40 -0300

    Create produto.java

---
+++ Lista 2/Ex 7/produto.java
@@ -0,0 +1,12 @@
+public  class  Produto {
+    String descricao;
+    preco duplo ;
+    int qtdEmEstoque;
+
+    // mostra informações gerais do produto
+    void  mostrarInfo () {
+        Sistema . para fora . println ( " Produto: " + descrição);
+        Sistema . para fora . println ( " Preço unitário: " + preco);
+        Sistema . para fora . println ( " Quantidade em estoque: " + qtdEmEstoque);
+    };
+}
