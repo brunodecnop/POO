8f600dd51f9baf4fc8ecc314f58a9571d8b7313e
brunodecnop <78354706+brunodecnop@users.noreply.github.com>
2021-06-28 18:11:19 -0300

    Create pessoa.java

---
+++ Lista 2/Ex 2/pessoa.java
@@ -0,0 +1,17 @@
+public  class  Pessoa {
+    Nome da string ;
+    int idade;
+
+    void  fazAniversario () {
+        idade ++ ;
+        Sistema . para fora . println ( " Feliz Aniversário de " + idade + " anos, " + nome + " ! " );
+    }
+
+    void  imprimirNome () {
+        Sistema . para fora . println ( " Nome: " + nome);
+    }
+
+    void  imprimirIdade () {
+        Sistema . para fora . println ( " A idade de " + nome + " é " + idade + " anos. " );
+    }
+}
