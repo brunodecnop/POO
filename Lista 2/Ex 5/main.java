a15467fce386863b6c01aa2d2c56a605c727ae90
brunodecnop <78354706+brunodecnop@users.noreply.github.com>
2021-06-28 18:18:10 -0300

    Create main.java

---
+++ Lista 2/Ex 5/main.java
@@ -0,0 +1,31 @@
+public  class  Main {
+    public  static  void  main ( String [] args ) {
+        Televisao televisao =  nova  Televisao ();
+        Sistema . para fora . println ( " Televisão \ n " );
+        televisao . displayTV ();
+        televisao . Ligado desligado();
+        televisao . decCanal ();
+        televisao . decCanal ();
+        televisao . decCanal ();
+        televisao . displayTV ();
+        televisao . defCanal ( 98 );
+        televisao . incCanal ();
+        televisao . incCanal ();
+        televisao . incCanal ();
+        televisao . incCanal ();
+        televisao . displayTV ();
+        televisao . incVolume ();
+        televisao . incVolume ();
+        televisao . incVolume ();
+        televisao . incVolume ();
+        televisao . displayTV ();
+        televisao . modoSilencioso ();
+        televisao . incVolume ();
+        televisao . incVolume ();
+        televisao . incVolume ();
+        televisao . displayTV ();
+        televisao . Ligado desligado();
+        televisao . displayTV ();
+    }
+    
+}
