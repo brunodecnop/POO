39427d6498e7c4e038f89c1f22a681ec829aae11
brunodecnop <78354706+brunodecnop@users.noreply.github.com>
2021-06-28 18:30:01 -0300

    Create cliente.java

---
+++ Lista 2/Ex 7/cliente.java
@@ -0,0 +1,12 @@
+public  class  Cliente {
+    Nome da string ;
+    String endereco;
+    String modoDePagamento; // indica o modo de pagamento do cliente
+
+    // mostra informações gerais do cliente
+    void  mostrarInfo () {
+        Sistema . para fora . println ( " Nome: " + nome);
+        Sistema . para fora . println ( " Endereço: " + endereco);
+        Sistema . para fora . println ( " Modo de Pagamento: " + modoDePagamento);
+    };
+}
