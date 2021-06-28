723e80ba5e91c6eabec20db7c0ac6616da1a0035
brunodecnop <78354706+brunodecnop@users.noreply.github.com>
2021-06-28 18:33:32 -0300

    Create bateria.java

---
+++ Lista 2/Ex 8/bateria.java
@@ -0,0 +1,15 @@
+class  Bateria {
+    int carga =  100 ;
+    
+    int  getCarga () {
+        carga de retorno ;
+    }
+    void  setCarga ( int  carga ) {
+        se (carga >  100 )
+            isso . carga =  100 ;
+        else  if (carga <  0 )
+            isso . carga =  0 ;
+        senão
+            isso . carga = carga;
+    }
+}
